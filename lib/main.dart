0c4ff405e2e5156adea22b150c87d181734d6d86
dongkwon <mono@mono-frame.com>
2025-12-24 09:01:08 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
+import 'package:toonflix/services/api_service.dart';
+
+void main() {
+  ApiSevice().getTodaysToons();
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomeScreen());
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 1,
+        surfaceTintColor: Colors.white,
+        shadowColor: Colors.black,
+        foregroundColor: Colors.green,
+        title: Text("오늘의 웹툰", style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+}
