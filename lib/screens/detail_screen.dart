854e7767faa4467d99a43f175061470c255e3d53
dongkwon <mono@mono-frame.com>
2025-12-25 19:13:41 +0900

    WebtoonEpisodeModel, WebtoonDetailModel 및 api 추가

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  final String title, thumb, id;
+
+  const DetailScreen({
+    super.key,
+    required this.title,
+    required this.thumb,
+    required this.id,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 1,
+        surfaceTintColor: Colors.white,
+        shadowColor: Colors.black,
+        foregroundColor: Colors.green,
+        title: Text(title, style: TextStyle(fontSize: 24)),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 50),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Hero(
+                tag: id,
+                child: Container(
+                  width: 250,
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 7,
+                        offset: Offset(2, 2),
+                        color: Colors.black.withAlpha(180),
+                      ),
+                    ],
+                  ),
+                  child: Image.network(
+                    thumb,
+                    headers: const {'Referer': 'https://comic.naver.com'},
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
